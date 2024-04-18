e3ff86655da9fd00b6cd54211529417a43c07dd9
eriarer <eriarer_messiah+git@hotmail.com>
2024-04-18 14:59:44 -0600

    test deletion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './components/test/test.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MascotaComponent } from './components/mascota/mascota.component';
+import { CitaComponent } from './components/cita/cita.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, NavbarComponent, InicioComponent, MascotaComponent, CitaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
