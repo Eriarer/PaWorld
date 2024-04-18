76d7c62105f5f62d84cd50b0d6e28c558e9ef350
eriarer <eriarer_messiah+git@hotmail.com>
2024-04-18 14:58:36 -0600

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CitaComponent } from './components/cita/cita.component';
+import { MascotaComponent } from './components/mascota/mascota.component';
+import { AgendaComponent } from './components/agenda/agenda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: InicioComponent},
+    {path: 'cita', component: CitaComponent},
+    {path: 'mascota', component: MascotaComponent},
+    {path: 'agenda', component: AgendaComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
