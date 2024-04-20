6d8f926d5c72faf42ce153062d81c5df72c8b38d
eriarer <eriarer_messiah+git@hotmail.com>
2024-04-19 18:22:21 -0600

    basic masonry desing (no more functionality)

--- src/app/components/mascota/mascota.component.ts
+++ src/app/components/mascota/mascota.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MascotasService } from '../../services/data/mascotas.service';
+import { Mascota } from '../../interfaces/mascota';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-mascota',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, ],
   templateUrl: './mascota.component.html',
   styleUrl: './mascota.component.css'
 })
 export class MascotaComponent {
+  mascotas: Mascota[];
 
+  constructor (public mascotasService: MascotasService){
+    this.mascotas = this.mascotasService.getMascotas();
+    console.log(this.mascotas);
+  }
+
+  formatoFecha(fecha: Date){
+    return formatDate(fecha, 'dd/MM/yyyy', 'en-US');
+  }
 }
