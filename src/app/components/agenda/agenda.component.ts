b3fe82071c20c9f281712ebb319534ab2d146870
mariana-avila-rivera <marianaavilarivera@gmail.com>
2024-04-21 16:54:31 -0600

    Uso de algunos sweatalert2, manejo de localstorage listo. Agregando sexo Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, Input } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MascotasService } from '../../services/data/mascotas.service';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {provideNativeDateAdapter} from '@angular/material/core';
@@ -20,6 +20,8 @@ import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerIntl } from '@angular/material/datepicker';
 import 'moment/locale/fr';
+import { CitasService } from '../../services/localstorage/citas.service';
+import Swal from 'sweetalert2';
 
 // Define custom date format for fr locale
 export const FR_DATE_FORMATS = {
@@ -64,16 +66,13 @@ export class AgendaComponent {
   telefono=0;
   //datos de la mascota
   @Input() mascota!: Mascota;
-  edad!:number;
-  color!:string;
-  raza!:string;
+
   tiempoRefugio!:number;
-  descripcion!:string;
-  imagen!:string;
 
   //datos para el calendario
   minDate!: Date;
   maxDate!: Date;
+
   //Para formatear la fecha (DD/MM/YYYY)
   private _adapter!: DateAdapter<any>;
   private _intl!: MatDatepickerIntl;
@@ -82,15 +81,16 @@ export class AgendaComponent {
   //Para almacennar datos de la cita
   dataAdoptante!: Adoptante;
   dataCita!: Cita;
+
   nombreAdop = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
   errorMessage = '';
   telAdop = new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}'), Validators.minLength(10),Validators.maxLength(10)]);
   errorMessage2 = '';
+  selectedDate: Date | null = null;
+  selectedHour: string | undefined;
 
-  constructor(public mascotasService: MascotasService) { 
+  constructor(public mascotasService: MascotasService, public citasService: CitasService) { 
     this.mascota = mascotasService.getMascotaById(1);
-    console.log(JSON.stringify(this.mascota));
-
     let fechaActual=new Date();
     //La fecha mínima es la fecha actual +1
     this.minDate=new Date(fechaActual.getFullYear(),fechaActual.getMonth(),fechaActual.getDate()+1);
@@ -108,15 +108,7 @@ export class AgendaComponent {
   }
 
   ngOnInit(): void {
-    this.edad=this.mascota.edad;
-    this.color=this.mascota.color;
-    this.raza=this.mascota.raza;
     this.tiempoRefugio=this.GettiempoRefugio();
-    this.descripcion=this.mascota.descripcion;
-    this.imagen=this.mascota.imagen;
-    console.log(this.imagen);
-
-    
   }
 
   //función para calcular el tiempo en el refugio del animal
@@ -124,9 +116,6 @@ export class AgendaComponent {
     let fechaActual=new Date();
     let fechaIngreso=new Date(this.mascota.fechaIngreso);
     let tiempoRefugio=fechaActual.getTime()-fechaIngreso.getTime();
-    console.log("fecha actual: "+fechaActual);
-    console.log("fecha de ingreso ingreso: "+fechaIngreso);
-    console.log("Tiempo en refugio: "+tiempoRefugio/(1000*60*60*24)+" días");
     return Math.floor(tiempoRefugio/(1000*60*60*24));
   }
 
@@ -139,12 +128,8 @@ export class AgendaComponent {
     else{
       this.mascota = this.mascotasService.getMascotaById(this.mascota.id+1);
     }
-    this.edad=this.mascota.edad;
-    this.color=this.mascota.color;
-    this.raza=this.mascota.raza;
     this.tiempoRefugio=this.GettiempoRefugio();
-    this.descripcion=this.mascota.descripcion;
-    this.imagen=this.mascota.imagen;
+
   }
 
   //Función para ir a la mascota anterior
@@ -155,12 +140,7 @@ export class AgendaComponent {
     else{
       this.mascota = this.mascotasService.getMascotaById(this.mascota.id-1);
     }
-    this.edad=this.mascota.edad;
-    this.color=this.mascota.color;
-    this.raza=this.mascota.raza;
     this.tiempoRefugio=this.GettiempoRefugio();
-    this.descripcion=this.mascota.descripcion;
-    this.imagen=this.mascota.imagen;
   }
 
   //Funciones para el manejo de errores
@@ -173,6 +153,8 @@ export class AgendaComponent {
       this.errorMessage = '';
     }
   }
+
+  
   updateErrorMessage2(){
     if (this.telAdop.hasError('required')) {
       this.errorMessage2 = 'Debes ingresar un valor';
@@ -183,6 +165,54 @@ export class AgendaComponent {
     }
   }
 
-  //Función para guardar datos de la cita en el localstorage
+  onDateChange(event: MatDatepickerInputEvent<Date>) {
+    this.selectedDate = event.value;
+  }
+
+  //Función para guardar datos de la cita en el localstorage con el service citas
+  GuardarCita() {
+    if(this.nombreAdop.invalid){
+      Swal.fire({
+        title: "Error!",
+        text: "Nombre invalido",
+        icon: "error"
+      });
+    }
+    else if(this.telAdop.invalid){
+      Swal.fire({
+        title: "Error!",
+        text: "Teléfono invalido",
+        icon: "error"
+      });
+    }else if(this.selectedDate==null){
+      Swal.fire({
+        title: "Error!",
+        text: "Debes seleccionar una fecha",
+        icon: "error"
+      });
+    }else if(this.selectedHour==null){
+      Swal.fire({
+        title: "Error!",
+        text: "Debes seleccionar una hora",
+        icon: "error"
+      });
+    }else{ //Todo es correcto
+      this.dataCita= this.citasService.newCita();
+      this.dataCita.fecha=this.selectedDate;
+      this.dataCita.hora={hours: Number(this.selectedHour.split(':')[0]), minutes: Number(this.selectedHour.split(':')[1])};
+      this.dataCita.adoptante.nombre=this.nombreAdop.value ?? '';
+      this.dataCita.adoptante.telefono=this.telAdop.value ?? '';
+      this.dataCita.mascota=this.mascota;
+      this.citasService.addCita(this.dataCita);
+      Swal.fire({
+        title: "Cita agendada",
+        text: "La cita ha sido agendada correctamente",
+        icon: "success"
+      });
+    }
+
+    
+   
+  }
   
 }
