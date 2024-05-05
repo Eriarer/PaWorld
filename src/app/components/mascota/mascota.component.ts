0c11db999bd8c2b216d3ca6b38972c9b47262e2d
eriarer <eriarer_messiah+git@hotmail.com>
2024-05-05 16:55:11 -0600

    refactor: Update router configuration for mascota component to use @Input()

--- src/app/components/mascota/mascota.component.ts
+++ src/app/components/mascota/mascota.component.ts
@@ -3,35 +3,21 @@ import { MascotasService } from '../../services/data/mascotas.service';
 import { Mascota } from '../../interfaces/mascota';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { formatDate } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mascota',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, RouterModule],
+  imports: [MatButtonModule, MatCardModule, RouterModule, CommonModule],
   templateUrl: './mascota.component.html',
   styleUrl: './mascota.component.css',
 })
 export class MascotaComponent {
   mascotas: Mascota[];
-  @Input() infoDesdeNavbar: any = null;
+  @Input('filter') infoDesdeNavbar!: string;
 
-  constructor(
-    public mascotasService: MascotasService,
-    private route: ActivatedRoute
-  ) {
+  constructor(public mascotasService: MascotasService) {
     this.mascotas = this.mascotasService.getMascotas();
-    console.log(this.mascotas);
-  }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      this.infoDesdeNavbar = params['infoDesdeNavbar'];
-    });
-  }
-
-  formatoFecha(fecha: Date) {
-    return formatDate(fecha, 'dd/MM/yyyy', 'en-US');
   }
 }
